1fc2aa3a2844b3beb5b8b7d0b11a1e870364bf63
dragos.marinescu <dragos.marinescu@aeratechnology.com>
2020-05-24 22:39:45 +0300

    change fnot

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./fonts/FiraSans-Regular.ttf";
 import "./index.css";
 
 import App from "./components/App";
@@ -10,5 +9,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
